7c3f12786dffcd7be69acad56cf31e111ccc1c8f
Felipe Ginklings <felipe.ginklings@gmail.com>
2023-04-09 18:46:49 -0300

    Creation of func 'get_complement'

--- main.py
+++ main.py
@@ -22,7 +22,23 @@ def pdf_to_list(filename: str = 'teste.pdf'):
     return total_text.split()
 
 
-def get_data(start: int = 0, pdf_list: List[str] = []):
+pdf_list = pdf_to_list()
+
+
+def get_complement(p_complemento, p_complemento_end):
+    try:
+        complement = int(pdf_list[p_complemento+2])
+    except ValueError:
+        extract_complement_number = [
+            n for n in pdf_list[p_complemento:p_complemento_end] if n.isnumeric()]
+        if not extract_complement_number:
+            complement = ' '.join(pdf_list[p_complemento:p_complemento_end])
+            return (True, complement)
+        complement = int(extract_complement_number[0])
+    return (False, complement)
+
+
+def get_data(start: int = 0):
     try:
         position_valor = pdf_list.index('Valor', start)
         position_hist = pdf_list.index('Hist', position_valor)
@@ -33,7 +49,8 @@ def get_data(start: int = 0, pdf_list: List[str] = []):
     value = float(
         pdf_list[position_valor+2].replace('.', '').replace(',', '.'))
     hist = int(pdf_list[position_hist+2])
-    has_error, complement = get_complement(position_complemento, position_complemento_end)
+    has_error, complement = get_complement(
+        position_complemento, position_complemento_end)
 
 
 def main():
