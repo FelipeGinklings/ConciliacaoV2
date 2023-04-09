92a41c0764829558a23b5622c20afaab5e608b5c
Felipe Ginklings <felipe.ginklings@gmail.com>
2023-04-09 18:55:41 -0300

    Creation of class 'Item'

---
+++ utils.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+from typing import Union
+
+
+@dataclass
+class Item:
+    value: float
+    hist: int
+    comp: Union[str, int]
+    has_error: bool
+
+    @property
+    def formated_value(self):
+        return '{:.2f}'.format(self.value).replace('.', ',')
